5d37d7448d79c0d1fbd25601f0109c71cf7d94f8
liucongyu1993@yahoo.com <liucongyu1993@yahoo.com>
2017-10-28 17:36:34 -0400

    merge

--- src/components/Gender.js
+++ src/components/Gender.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import './submit.css';
 
 const Gender = ({onChange,onSubmit}) => {
 	return(
 		<div>
-			<div >
+            <div >
 				What is your recipient's gender?
 				<form onSubmit={onSubmit}>
 					<input type="text" onChange={onChange}/>
 					<button>Submit</button>
 				</form>
 
-			</div>
+				<div class="sex">
+                    <img class="male" draggable="false" src="src/components/asset/93.png"/>
+						<p>Male</p>
+                    <img class="female" draggable="false" src=""/>
+                        <p>Female</p>
+				</div>
+
+            </div>
 		</div>
 	)
 };
